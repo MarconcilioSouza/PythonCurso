a5af1af7c035d0a834067d1bfbb429a1ca772873
marconcilioka <marconcilioka@kalunga.com.br>
2020-06-08 17:34:24 -0300

    novos arquivos

--- flask-masterclass/template_engine/app.py
+++ flask-masterclass/template_engine/app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="tema")
+app.config["SECRET_KEY"] = "secret"
 
 
 @app.route("/templates")
 def template():
-    return render_template("index.html")
+    users = {
+        "name": "Marconcilio Souza",
+        "idade": 35,
+        "email": "marco@hotmail.com"
+    }
+
+    flash("usuário criado com sucesso!")
+    return render_template("index.html", users=users)
+
+
+@app.route("/users")
+def users():
+    flash("usuário criado com sucesso!")
+
+    return render_template("users.html")
 
 
 if __name__ == "__main__":

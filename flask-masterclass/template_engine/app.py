27293a38b6ccf938625c3b7754b7cf7283cf70ef
marconcilioka <marconcilioka@kalunga.com.br>
2020-06-09 10:18:22 -0300

    continuação modulo 5

--- flask-masterclass/template_engine/app.py
+++ flask-masterclass/template_engine/app.py
@@ -1,26 +1,33 @@
 from flask import Flask, render_template, flash
 
-app = Flask(__name__, template_folder="tema")
+app = Flask(__name__, 
+template_folder="tema", 
+static_folder="public")
 app.config["SECRET_KEY"] = "secret"
 
 
 @app.route("/templates")
 def template():
-    users = {
+    user_page = True
+    return render_template("index.html", user_page=user_page)
+
+
+@app.route("/Usuarios")
+def users():
+    users = [{
         "name": "Marconcilio Souza",
         "idade": 35,
-        "email": "marco@hotmail.com"
-    }
-
+        "email": "marco@hotmail.com",
+        "active": True
+    },
+    {
+        "name": "Amanda Souza",
+        "idade": 28,
+        "email": "oi@hotmail.com",
+        "active": False
+    }]
     flash("usuário criado com sucesso!")
-    return render_template("index.html", users=users)
-
-
-@app.route("/users")
-def users():
-    flash("usuário criado com sucesso!")
-
-    return render_template("users.html")
+    return render_template("users.html", users=users)
 
 
 if __name__ == "__main__":

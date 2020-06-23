0613d65efcc0200b39023d57434ac217223aaf56
marconcilioka <marconcilioka@kalunga.com.br>
2020-06-23 14:02:30 -0300

    finalização modulo 5

--- flask-masterclass/05-forms/app/__init__.py
+++ flask-masterclass/05-forms/app/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 bootstrap = Bootstrap()
-
 
 def create_app():
     app = Flask(__name__)
--- flask-masterclass/05-forms/app/routes.py
+++ flask-masterclass/05-forms/app/routes.py
@@ -1,11 +1,17 @@
-from flask import flash, redirect, render_template, request, url_for
-from flask_login import login_required, login_user, logout_user
+from datetime import timedelta
+
+from flask import flash, redirect, render_template, url_for
+from flask_login import (
+    login_required, 
+    login_user, 
+    logout_user,
+    current_user
+)
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db
-from app.models import User
-from datetime import timedelta
-from app.forms import LoginForm, RegisterForm
+from app.models import User, Book
+from app.forms import LoginForm, RegisterForm, BookForm, UserBookForm
 
 
 def init_app(app):
@@ -14,13 +20,11 @@ def init_app(app):
         users = User.query.all() # Select * from users; 
         return render_template("users.html", users=users)
 
-
     @app.route("/user/<int:id>")
     @login_required
     def unique(id):
         user = User.query.get(id)
         return render_template("user.html", user=user)
-
 
     @app.route("/user/delete/<int:id>")
     def delete(id):
@@ -43,7 +47,6 @@ def init_app(app):
             db.session.add(user)
             db.session.commit()
             return redirect(url_for("index"))
-
         return render_template("register.html", form=form)
 
     @app.route("/login", methods=["GET", "POST"])
@@ -61,14 +64,44 @@ def init_app(app):
                 flash("Credênciais incorretas", "danger")
                 return redirect(url_for("login"))
 
-            login_user(user, remember=form.remenber.data, duration=timedelta(days=7))
+            login_user(user, remember=form.remember.data, duration=timedelta(days=7))
             return redirect(url_for("index"))
 
         return render_template("login.html", form=form)
-
 
     @app.route("/logout")
     @login_required
     def logout():
         logout_user()
         return redirect(url_for("index"))
+
+    @app.route("/book/add", methods=["GET", "POST"])
+    def book_add():
+        form = BookForm()
+
+        if form.validate_on_submit():
+            book = Book()
+            book.name = form.name.data
+
+            db.session.add(book)
+            db.session.commit()
+
+            flash("Livro cadastrado com sucesso", "success")
+            return redirect(url_for("book_add"))
+        return render_template("book/add.html", form=form)
+
+    @app.route("/user/<int:id>/add-book",  methods=["GET", "POST"])
+    def user_add_book(id):
+        form = UserBookForm()
+
+        if form.validate_on_submit():
+            book = Book.query.get(form.book.data)
+            current_user.books.append(book)
+
+            db.session.add(current_user)
+            db.session.commit()
+
+            flash("Livro cadastrado com sucesso!", "success")
+            return redirect(url_for("user_add_book", id=current_user.id))
+
+        return render_template("book/user_add_book.html", form=form)
